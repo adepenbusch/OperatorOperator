d6e4a8c03a1d1f52566e06fb8ec9f0dc9bf902aa
Alex Z <saaqibz@gmail.com>
2018-01-27 15:44:46 -0500

    more stuff

--- Switchboard/Assets/scripts/GameController.cs
+++ Switchboard/Assets/scripts/GameController.cs
@@ -16,7 +16,7 @@ public class GameController : MonoBehaviour {
 	private void InstantiatePlugs() {
 		for (var col = 0 ; col < 4; ++col) {
 			for (var row = 0; row < 4; ++row) {
-				var position = new Vector3(-3.5f + colSpace * col, 3.5f + rowSpace * -1 * row, 0f);		
+				var position = new Vector3(-3.5f + colSpace * col, 3.5f + -1 * rowSpace * row, 0f);		
 				Instantiate(plugPrefab, position, Quaternion.Euler(90, 0, 0));
 			}
 		}
