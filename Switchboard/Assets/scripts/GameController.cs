098d565cc4669493baaa55cdd64ae339dcf52ede
Alex Z <saaqibz@gmail.com>
2018-01-27 16:52:40 -0500

    added plug selection logic

--- Switchboard/Assets/scripts/GameController.cs
+++ Switchboard/Assets/scripts/GameController.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameController : MonoBehaviour {
+	private const int NUM_COLS = 4;
+	private const int NUM_ROWS = 4;
 
 	public GameObject plugPrefab;
 	public float colSpace;
 	public float rowSpace;
+
+	private GameObject[,] plugGrid = new GameObject[NUM_COLS, NUM_ROWS];
+	private GameObject startPlug;
+	private GameObject endPlug;
+
 
 	// Use this for initialization
 	void Start () {
@@ -14,14 +21,35 @@ public class GameController : MonoBehaviour {
 	}
 
 	private void InstantiatePlugs() {
-		for (var col = 0 ; col < 4; ++col) {
-			for (var row = 0; row < 4; ++row) {
+		for (var col = 0 ; col < NUM_COLS; ++col) {
+			for (var row = 0; row < NUM_ROWS; ++row) {
 				var position = new Vector3(-3.5f + colSpace * col, 3.5f + -1 * rowSpace * row, 0f);		
-				Instantiate(plugPrefab, position, Quaternion.Euler(90, 0, 0));
+				var plug = Instantiate(plugPrefab, position, Quaternion.Euler(90, 0, 0));
+				plug.GetComponent<Plug>().setPosition(col, row);
+				plugGrid [col,row] = plug;
 			}
 		}
 	}
-	
+
+	public void triggerPlug(GameObject plug) {
+		if (plug == startPlug) {
+			startPlug = null;
+		} else if (plug == endPlug) {
+			endPlug = null;
+		} else if (startPlug == null) {
+			startPlug = plug;
+		} else if (endPlug == null) {
+			endPlug = plug;
+		} else {
+			// Do nothing because both plugs are assigned
+		}
+
+
+		var _startPlug = (startPlug != null) ? startPlug.GetComponent<Plug> ().ToString () : "null";
+		var _endPlug = (endPlug != null) ? endPlug.GetComponent<Plug> ().ToString () : "null";
+		Debug.Log ("startPlug: " + _startPlug + "| endPlug: " + _endPlug);
+	}
+
 	// Update is called once per frame
 	void Update () {
 		
--- Switchboard/Assets/scripts/Plug.cs
+++ Switchboard/Assets/scripts/Plug.cs
@@ -3,18 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Plug : MonoBehaviour {
+	
+	public static GameObject gameController;
 
-	public int row;
-	public int column;
-
+	private int row = -1;
+	private int col = -1;
 
 	// Use this for initialization
 	void Start () {
-		
+		if (gameController == null) {
+			gameController = GameObject.Find ("GameController");
+		}
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	void OnMouseDown(){
+		// this object was clicked - do something
+		gameController.GetComponent<GameController>().triggerPlug(gameObject);
+	}
+
+	public void setPosition(int x, int y) {
+		col = x;
+		row = y;
+	}
+
+	public override string ToString() {
+		return "Plug(" + col + ", " + row + ")";
 	}
 }
